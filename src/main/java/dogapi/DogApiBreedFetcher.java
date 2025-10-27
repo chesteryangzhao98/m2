951b4072302cc4720ec3aa67c607a2207c747206
chester zhao <chester.zhao@mail.utoronto.ca>
2025-10-27 16:26:51 -0400

    comment change

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -38,12 +38,12 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 throw new BreedNotFoundException("HTTP error: " + response.code());
             }
 
-            // analyse Json response
+            // analyse Json responses
             assert response.body() != null;
             String responseBody = response.body().string();
             JSONObject json = new JSONObject(responseBody);
 
-            // check api response
+            // check api responses
             String status = json.getString("status");
             if ("error".equals(status)) {
                 throw new BreedNotFoundException(breed);
